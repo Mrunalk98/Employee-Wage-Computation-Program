9d613e4f87e9984a0f3bb6e8fa459fd308de4753
Kmrunal20 <mrunal.kurhade@digiplusit.com>
2020-09-28 22:26:51 +0530

    [mrunal] Refactor. Added multiple companies

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -9,7 +9,9 @@ namespace EmployeeWages
             Console.WriteLine("Welcome to Employee Wage Computation Program");
 
             EmployeeWageCalc capgemini = new EmployeeWageCalc("Capgemini", 10, 12, 40);
+            EmployeeWageCalc jio = new EmployeeWageCalc("Jio", 10, 8, 50);
             capgemini.CalcTotalEmpWage();
+            jio.CalcTotalEmpWage();
         }
 
     }
