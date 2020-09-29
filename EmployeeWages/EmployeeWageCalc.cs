4c9b6de1ef006caaad5601d35d9da9e0b61769b2
Kmrunal20 <mrunal.kurhade@digiplusit.com>
2020-09-29 10:45:07 +0530

    [mrunal] Add. Computed Employee wage for multiple companies using array

--- EmployeeWages/EmployeeWageCalc.cs
+++ EmployeeWages/EmployeeWageCalc.cs
@@ -10,19 +10,8 @@ namespace EmployeeWages
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
 
-        int empRatePerHour;
-        int noOfWorkingDays;
-        int noOfWorkingHours;
-        string companyName;
 
-        public EmployeeWageCalc(string COMPANY_NAME, int EMP_RATE_PER_HOUR, int NO_OF_WORKING_DAYS, int NO_OF_WORKING_HOURS)
-        {
-            this.companyName = COMPANY_NAME;
-            this.empRatePerHour = EMP_RATE_PER_HOUR;
-            this.noOfWorkingDays = NO_OF_WORKING_DAYS;
-            this.noOfWorkingHours = NO_OF_WORKING_HOURS;
-        }
-        public int CalcTotalEmpWage()
+        public int CalcTotalEmpWage(Company company)
         {
 
             int empHrs;
@@ -30,7 +19,7 @@ namespace EmployeeWages
             int totalEmpHrs = 0;
             int totalWorkingDays = 0;
 
-            for (totalWorkingDays = 1; totalWorkingDays <= this.noOfWorkingDays && totalEmpHrs <= this.noOfWorkingHours; totalWorkingDays++)
+            for (totalWorkingDays = 1; totalWorkingDays <= company.noOfWorkingDays && totalEmpHrs <= company.noOfWorkingHours; totalWorkingDays++)
             {
 
                 Random random = new Random();
@@ -51,9 +40,10 @@ namespace EmployeeWages
                         break;
                 }
                 totalEmpHrs += empHrs;
+                Console.WriteLine("Days : " + totalWorkingDays + ", Emp hours : " + empHrs);
             }
-            totalWage = totalEmpHrs * this.empRatePerHour;
-            Console.WriteLine("Total Employee Wage of " + this.companyName + " : " + totalWage);
+            totalWage = totalEmpHrs * company.empRatePerHour;
+            Console.WriteLine("\nTotal Employee Wage of " + company.companyName + " : " + totalWage + "\n");
             return totalWage;
         }
     }
--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -4,14 +4,30 @@ namespace EmployeeWages
 {
     class Program
     {
+        private static Company[] companyArray = new Company[10];
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
 
-            EmployeeWageCalc capgemini = new EmployeeWageCalc("Capgemini", 10, 12, 40);
-            EmployeeWageCalc jio = new EmployeeWageCalc("Jio", 10, 8, 50);
-            capgemini.CalcTotalEmpWage();
-            jio.CalcTotalEmpWage();
+            Console.WriteLine("Enter the number of companies : ");
+            var num = Convert.ToInt32(Console.ReadLine());
+
+            EmployeeWageCalc empWageCalc = new EmployeeWageCalc();
+
+            for (int i = 1; i <= num; i++)
+            {
+                Console.WriteLine("Enter data of company " + i);
+                Console.WriteLine("Enter company name : ");
+                var companyName = Console.ReadLine();
+                Console.WriteLine("Enter employee rate per hour: ");
+                var empRatePerHour = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter number of working days : ");
+                var noOfWorkingDays = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter number of working hours : ");
+                var noOfWorkingHours = Convert.ToInt32(Console.ReadLine());
+                companyArray[i] = new Company(companyName, empRatePerHour, noOfWorkingDays, noOfWorkingHours);
+                empWageCalc.CalcTotalEmpWage(companyArray[i]);
+            }
         }
 
     }
